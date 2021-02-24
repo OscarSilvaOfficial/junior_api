1c3c94e4fdf3162195901c0d1338fa24207a66f0
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-02-23 22:00:38 -0300

    Deploy no heroku

--- core/settings.py
+++ core/settings.py
@@ -96,6 +96,7 @@ STATIC_URL = '/static/'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    'https://junior-web-oscarsilvaofficial.vercel.app/'
     "https://junior-web.vercel.app",
     "https://junior-web-git-master-oscarsilvaofficial.vercel.app/"
 ]
